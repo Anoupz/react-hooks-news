d25294999c2c65093cb7910451aa24406441e3d6
Anoop Narayanan <anoop.narayanan@doordash.com>
2020-05-24 00:11:35 -0700

    More hooks with api

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
+const App: React.FC = () => {
+  const [results, setResults] = useState([]);
+  const [query, setQuery] = useState("react hooks");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<{ message: "" } | null>(null);
+
+  const searchInputRef = useRef<HTMLInputElement>(null);
+
+  const getResults = async () => {
+    setLoading(true);
+    try {
+      const {
+        data: { hits },
+      } = await axios.get(`http://hn.algolia.com/api/v1/search?query=${query}`);
+      setResults(hits);
+    } catch (e) {
+      setError(e);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getResults();
+  }, []);
+
+  const handleSearch = (event: React.SyntheticEvent<EventTarget>) => {
+    event.preventDefault();
+    getResults();
+  };
+
+  const handleClearSearch = () => {
+    setQuery("");
+    searchInputRef?.current?.focus();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSearch}>
+        <input
+          type="text"
+          value={query}
+          ref={searchInputRef}
+          onChange={(event) => setQuery(event.target.value)}
+        />
+        <button type="submit">Search</button>
+        <button type="button" onClick={handleClearSearch}>
+          Clear
+        </button>
+      </form>
+      <br />
+      {loading ? (
+        <div>Loading results...</div>
+      ) : (
+        <ul>
+          {results.map(
+            (result: { objectID: string; url: string; title: string }) => (
+              <li key={result.objectID}>
+                <a href={result.url}>{result.title}</a>
+              </li>
+            )
+          )}
+        </ul>
+      )}
+      {error && <div>{error.message}</div>}
     </div>
   );
-}
+};
 
 export default App;
